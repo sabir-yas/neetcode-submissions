c63fc591ed65f5c5a51de1909393b811aa001644
Yaseer Sabir <84865370+sabir-yas@users.noreply.github.com>
2026-05-23 08:08:55 -0600

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.py
@@ -0,0 +1,122 @@
+
+'''
+class TimeMap:
+
+
+    def __init__(self):
+        self.timeMap = {} #key= string, value = list of [val, timestamp]
+
+    def set(self, key: str, value: str, timestamp: int) -> None: # O(1) approach    
+        # 1: ["alice", "happy"],
+        if key not in self.timeMap:
+            self.timeMap[key] = []
+        self.timeMap[key].append([value,timestamp])
+        
+    def get(self, key: str, timestamp: int) -> str: # From O(n) to O(log n) binary approach
+        res = ""    
+        values = self.timeMap.get(key, []) # the [] is becuz if the key doesn't exist in the hashmap
+
+        #binary search
+        l, r = 0, len(values) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+            if values[mid][1] <= timestamp:
+                res = values[mid][0]
+                l = mid + 1
+            else:
+                r = mid - 1
+        return res
+
+'''
+
+
+ 
+
+'''
+ {
+    "alice": [[1, "happy"], [3,"sad"]]
+    "bob": [[1,"crazy"], [2,"angry"]]
+ }
+'''
+
+class TimeMap:
+
+    def __init__(self):
+        self.store= defaultdict(list) #[index, value]
+    
+    def set(self, key:str, value:str, timestamp: int) ->None:
+        self.store[key].append([timestamp, value])
+    
+    def get(self, key:str, timestamp: int)->str:
+        res = ""
+        if not self.store.get(key, ""):
+            return ""
+
+        l, r=0, len(self.store[key])-1
+        while l<=r:
+            mid =(l+r)//2
+            
+            if(self.store[key][mid][0] > timestamp):
+                r = mid-1
+            elif(self.store[key][mid][0] <= timestamp):
+                res = self.store[key][mid][1]
+                l = mid+1
+        return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
