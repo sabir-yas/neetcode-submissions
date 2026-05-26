a281151e55f4c06bf40fe1cd5bf9b1fa2eb072f3
Yaseer Sabir <84865370+sabir-yas@users.noreply.github.com>
2026-05-25 19:29:21 -0600

    Add: buy-and-sell-crypto - submission-7

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-7.py
@@ -0,0 +1,73 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        #brute force
+        '''
+        diff = 0
+        Maxdiff =0
+
+        for i in range(len(prices)):
+            for j in range(i+1, len(prices)):
+                diff = prices[j] - prices[i]
+                Maxdiff = max(Maxdiff, diff)
+        
+        return Maxdiff
+
+        '''
+
+        #optimal- O(n) Time and O(1) space- Sliding window
+        
+        l = 1
+        currMin = prices[0]
+        maxP = 0
+
+        while l < len(prices):
+            diff = prices[l] - currMin
+            currMin = min(currMin, prices[l])
+            maxP = max(maxP, diff)
+            l+=1
+        
+        return maxP
+
+        '''
+        l, r = 0, 1 #left= buy, right = sell
+        maxP =0
+
+        while r < len(prices):
+            #profitable ?
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                maxP = max(maxP, profit)
+            else:
+                l = r
+            r+=1
+        '''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
