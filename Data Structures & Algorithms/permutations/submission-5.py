a3282aa4c379ccfbe3b628960a5f6dc0aba0f3e7
Yaseer Sabir <84865370+sabir-yas@users.noreply.github.com>
2026-07-18 00:01:13 -0600

    Add: permutations - submission-5

---
+++ Data Structures & Algorithms/permutations/submission-5.py
@@ -0,0 +1,16 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        
+        res = []
+        if len(nums) ==0:
+            return [[]]
+
+        trace = self.permute(nums[1:])
+
+        for p in trace:
+            for i in range(len(p) + 1):
+                p_copy = p.copy() # always a list
+                p_copy.insert(i, nums[0])
+                res.append(p_copy)
+        
+        return res
